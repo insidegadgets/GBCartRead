69a49a5c79a38a75f3c8cc0bf3999f3dc51a5b47
alex@insidegadgets.com <alex@insidegadgets.com>
2014-11-14 20:47:23 +1000

    V1.4.2, files added

---
+++ GBCartRead_v1_4_2_C/gbcartread/rs232.c
@@ -0,0 +1,539 @@
+/*
+***************************************************************************
+*
+* Author: Teunis van Beelen
+*
+* Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013 Teunis van Beelen
+*
+* teuniz@gmail.com
+*
+***************************************************************************
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation version 2 of the License.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+***************************************************************************
+*
+* This version of GPL is at http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+*
+***************************************************************************
+*/
+
+/* last revision: February 1, 2013 */
+
+/* For more info and how to use this libray, visit: http://www.teuniz.net/RS-232/ */
+
+
+#include "rs232.h"
+
+
+
+#ifdef __linux__   /* Linux */
+
+
+int Cport[30],
+    error;
+
+struct termios new_port_settings,
+       old_port_settings[30];
+
+char comports[30][16]={"/dev/ttyS0","/dev/ttyS1","/dev/ttyS2","/dev/ttyS3","/dev/ttyS4","/dev/ttyS5",
+                       "/dev/ttyS6","/dev/ttyS7","/dev/ttyS8","/dev/ttyS9","/dev/ttyS10","/dev/ttyS11",
+                       "/dev/ttyS12","/dev/ttyS13","/dev/ttyS14","/dev/ttyS15","/dev/ttyUSB0",
+                       "/dev/ttyUSB1","/dev/ttyUSB2","/dev/ttyUSB3","/dev/ttyUSB4","/dev/ttyUSB5",
+                       "/dev/ttyAMA0","/dev/ttyAMA1","/dev/ttyACM0","/dev/ttyACM1",
+                       "/dev/rfcomm0","/dev/rfcomm1","/dev/ircomm0","/dev/ircomm1"};
+
+
+
+int RS232_OpenComport(int comport_number, int baudrate)
+{
+  int baudr, status;
+
+  if((comport_number>29)||(comport_number<0))
+  {
+    printf("illegal comport number\n");
+    return(1);
+  }
+
+  switch(baudrate)
+  {
+    case      50 : baudr = B50;
+                   break;
+    case      75 : baudr = B75;
+                   break;
+    case     110 : baudr = B110;
+                   break;
+    case     134 : baudr = B134;
+                   break;
+    case     150 : baudr = B150;
+                   break;
+    case     200 : baudr = B200;
+                   break;
+    case     300 : baudr = B300;
+                   break;
+    case     600 : baudr = B600;
+                   break;
+    case    1200 : baudr = B1200;
+                   break;
+    case    1800 : baudr = B1800;
+                   break;
+    case    2400 : baudr = B2400;
+                   break;
+    case    4800 : baudr = B4800;
+                   break;
+    case    9600 : baudr = B9600;
+                   break;
+    case   19200 : baudr = B19200;
+                   break;
+    case   38400 : baudr = B38400;
+                   break;
+    case   57600 : baudr = B57600;
+                   break;
+    case  115200 : baudr = B115200;
+                   break;
+    case  230400 : baudr = B230400;
+                   break;
+    case  460800 : baudr = B460800;
+                   break;
+    case  500000 : baudr = B500000;
+                   break;
+    case  576000 : baudr = B576000;
+                   break;
+    case  921600 : baudr = B921600;
+                   break;
+    case 1000000 : baudr = B1000000;
+                   break;
+    default      : printf("invalid baudrate\n");
+                   return(1);
+                   break;
+  }
+
+  Cport[comport_number] = open(comports[comport_number], O_RDWR | O_NOCTTY | O_NDELAY);
+  if(Cport[comport_number]==-1)
+  {
+    perror("unable to open comport ");
+    return(1);
+  }
+
+  error = tcgetattr(Cport[comport_number], old_port_settings + comport_number);
+  if(error==-1)
+  {
+    close(Cport[comport_number]);
+    perror("unable to read portsettings ");
+    return(1);
+  }
+  memset(&new_port_settings, 0, sizeof(new_port_settings));  /* clear the new struct */
+
+  new_port_settings.c_cflag = baudr | CS8 | CLOCAL | CREAD;
+  new_port_settings.c_iflag = IGNPAR;
+  new_port_settings.c_oflag = 0;
+  new_port_settings.c_lflag = 0;
+  new_port_settings.c_cc[VMIN] = 0;      /* block untill n bytes are received */
+  new_port_settings.c_cc[VTIME] = 0;     /* block untill a timer expires (n * 100 mSec.) */
+  error = tcsetattr(Cport[comport_number], TCSANOW, &new_port_settings);
+  if(error==-1)
+  {
+    close(Cport[comport_number]);
+    perror("unable to adjust portsettings ");
+    return(1);
+  }
+
+  if(ioctl(Cport[comport_number], TIOCMGET, &status) == -1)
+  {
+    perror("unable to get portstatus");
+    return(1);
+  }
+
+  status |= TIOCM_DTR;    /* turn on DTR */
+  status |= TIOCM_RTS;    /* turn on RTS */
+
+  if(ioctl(Cport[comport_number], TIOCMSET, &status) == -1)
+  {
+    perror("unable to set portstatus");
+    return(1);
+  }
+
+  return(0);
+}
+
+
+int RS232_PollComport(int comport_number, unsigned char *buf, int size)
+{
+  int n;
+
+#ifndef __STRICT_ANSI__                       /* __STRICT_ANSI__ is defined when the -ansi option is used for gcc */
+  if(size>SSIZE_MAX)  size = (int)SSIZE_MAX;  /* SSIZE_MAX is defined in limits.h */
+#else
+  if(size>4096)  size = 4096;
+#endif
+
+  n = read(Cport[comport_number], buf, size);
+
+  return(n);
+}
+
+
+int RS232_SendByte(int comport_number, unsigned char byte)
+{
+  int n;
+
+  n = write(Cport[comport_number], &byte, 1);
+  if(n<0)  return(1);
+
+  return(0);
+}
+
+
+int RS232_SendBuf(int comport_number, unsigned char *buf, int size)
+{
+  return(write(Cport[comport_number], buf, size));
+}
+
+
+void RS232_CloseComport(int comport_number)
+{
+  int status;
+
+  if(ioctl(Cport[comport_number], TIOCMGET, &status) == -1)
+  {
+    perror("unable to get portstatus");
+  }
+
+  status &= ~TIOCM_DTR;    /* turn off DTR */
+  status &= ~TIOCM_RTS;    /* turn off RTS */
+
+  if(ioctl(Cport[comport_number], TIOCMSET, &status) == -1)
+  {
+    perror("unable to set portstatus");
+  }
+
+  close(Cport[comport_number]);
+  tcsetattr(Cport[comport_number], TCSANOW, old_port_settings + comport_number);
+}
+
+/*
+Constant  Description
+TIOCM_LE  DSR (data set ready/line enable)
+TIOCM_DTR DTR (data terminal ready)
+TIOCM_RTS RTS (request to send)
+TIOCM_ST  Secondary TXD (transmit)
+TIOCM_SR  Secondary RXD (receive)
+TIOCM_CTS CTS (clear to send)
+TIOCM_CAR DCD (data carrier detect)
+TIOCM_CD  Synonym for TIOCM_CAR
+TIOCM_RNG RNG (ring)
+TIOCM_RI  Synonym for TIOCM_RNG
+TIOCM_DSR DSR (data set ready)
+*/
+
+int RS232_IsCTSEnabled(int comport_number)
+{
+  int status;
+
+  ioctl(Cport[comport_number], TIOCMGET, &status);
+
+  if(status&TIOCM_CTS) return(1);
+  else return(0);
+}
+
+int RS232_IsDSREnabled(int comport_number)
+{
+  int status;
+
+  ioctl(Cport[comport_number], TIOCMGET, &status);
+
+  if(status&TIOCM_DSR) return(1);
+  else return(0);
+}
+
+void RS232_enableDTR(int comport_number)
+{
+  int status;
+
+  if(ioctl(Cport[comport_number], TIOCMGET, &status) == -1)
+  {
+    perror("unable to get portstatus");
+  }
+
+  status |= TIOCM_DTR;    /* turn on DTR */
+
+  if(ioctl(Cport[comport_number], TIOCMSET, &status) == -1)
+  {
+    perror("unable to set portstatus");
+  }
+}
+
+void RS232_disableDTR(int comport_number)
+{
+  int status;
+
+  if(ioctl(Cport[comport_number], TIOCMGET, &status) == -1)
+  {
+    perror("unable to get portstatus");
+  }
+
+  status &= ~TIOCM_DTR;    /* turn off DTR */
+
+  if(ioctl(Cport[comport_number], TIOCMSET, &status) == -1)
+  {
+    perror("unable to set portstatus");
+  }
+}
+
+void RS232_enableRTS(int comport_number)
+{
+  int status;
+
+  if(ioctl(Cport[comport_number], TIOCMGET, &status) == -1)
+  {
+    perror("unable to get portstatus");
+  }
+
+  status |= TIOCM_RTS;    /* turn on RTS */
+
+  if(ioctl(Cport[comport_number], TIOCMSET, &status) == -1)
+  {
+    perror("unable to set portstatus");
+  }
+}
+
+void RS232_disableRTS(int comport_number)
+{
+  int status;
+
+  if(ioctl(Cport[comport_number], TIOCMGET, &status) == -1)
+  {
+    perror("unable to get portstatus");
+  }
+
+  status &= ~TIOCM_RTS;    /* turn off RTS */
+
+  if(ioctl(Cport[comport_number], TIOCMSET, &status) == -1)
+  {
+    perror("unable to set portstatus");
+  }
+}
+
+
+#else         /* windows */
+
+
+HANDLE Cport[16];
+
+
+char comports[16][10]={"\\\\.\\COM1",  "\\\\.\\COM2",  "\\\\.\\COM3",  "\\\\.\\COM4",
+                       "\\\\.\\COM5",  "\\\\.\\COM6",  "\\\\.\\COM7",  "\\\\.\\COM8",
+                       "\\\\.\\COM9",  "\\\\.\\COM10", "\\\\.\\COM11", "\\\\.\\COM12",
+                       "\\\\.\\COM13", "\\\\.\\COM14", "\\\\.\\COM15", "\\\\.\\COM16"};
+
+char baudr[64];
+
+
+int RS232_OpenComport(int comport_number, int baudrate)
+{
+  if((comport_number>15)||(comport_number<0))
+  {
+    printf("illegal comport number\n");
+    return(1);
+  }
+
+  switch(baudrate)
+  {
+    case     110 : strcpy(baudr, "baud=110 data=8 parity=N stop=1 dtr=on rts=on");
+                   break;
+    case     300 : strcpy(baudr, "baud=300 data=8 parity=N stop=1 dtr=on rts=on");
+                   break;
+    case     600 : strcpy(baudr, "baud=600 data=8 parity=N stop=1 dtr=on rts=on");
+                   break;
+    case    1200 : strcpy(baudr, "baud=1200 data=8 parity=N stop=1 dtr=on rts=on");
+                   break;
+    case    2400 : strcpy(baudr, "baud=2400 data=8 parity=N stop=1 dtr=on rts=on");
+                   break;
+    case    4800 : strcpy(baudr, "baud=4800 data=8 parity=N stop=1 dtr=on rts=on");
+                   break;
+    case    9600 : strcpy(baudr, "baud=9600 data=8 parity=N stop=1 dtr=on rts=on");
+                   break;
+    case   19200 : strcpy(baudr, "baud=19200 data=8 parity=N stop=1 dtr=on rts=on");
+                   break;
+    case   38400 : strcpy(baudr, "baud=38400 data=8 parity=N stop=1 dtr=on rts=on");
+                   break;
+    case   57600 : strcpy(baudr, "baud=57600 data=8 parity=N stop=1 dtr=on rts=on");
+                   break;
+    case  115200 : strcpy(baudr, "baud=115200 data=8 parity=N stop=1 dtr=on rts=on");
+                   break;
+    case  128000 : strcpy(baudr, "baud=128000 data=8 parity=N stop=1 dtr=on rts=on");
+                   break;
+    case  256000 : strcpy(baudr, "baud=256000 data=8 parity=N stop=1 dtr=on rts=on");
+                   break;
+    case  500000 : strcpy(baudr, "baud=500000 data=8 parity=N stop=1 dtr=on rts=on");
+                   break;
+    case 1000000 : strcpy(baudr, "baud=1000000 data=8 parity=N stop=1 dtr=on rts=on");
+                   break;
+    default      : printf("invalid baudrate\n");
+                   return(1);
+                   break;
+  }
+
+  Cport[comport_number] = CreateFileA(comports[comport_number],
+                      GENERIC_READ|GENERIC_WRITE,
+                      0,                          /* no share  */
+                      NULL,                       /* no security */
+                      OPEN_EXISTING,
+                      0,                          /* no threads */
+                      NULL);                      /* no templates */
+
+  if(Cport[comport_number]==INVALID_HANDLE_VALUE)
+  {
+    printf("unable to open comport\n");
+    return(1);
+  }
+
+  DCB port_settings;
+  memset(&port_settings, 0, sizeof(port_settings));  /* clear the new struct  */
+  port_settings.DCBlength = sizeof(port_settings);
+
+  if(!BuildCommDCBA(baudr, &port_settings))
+  {
+    printf("unable to set comport dcb settings\n");
+    CloseHandle(Cport[comport_number]);
+    return(1);
+  }
+
+  if(!SetCommState(Cport[comport_number], &port_settings))
+  {
+    printf("unable to set comport cfg settings\n");
+    CloseHandle(Cport[comport_number]);
+    return(1);
+  }
+
+  COMMTIMEOUTS Cptimeouts;
+
+  Cptimeouts.ReadIntervalTimeout         = MAXDWORD;
+  Cptimeouts.ReadTotalTimeoutMultiplier  = 0;
+  Cptimeouts.ReadTotalTimeoutConstant    = 0;
+  Cptimeouts.WriteTotalTimeoutMultiplier = 0;
+  Cptimeouts.WriteTotalTimeoutConstant   = 0;
+
+  if(!SetCommTimeouts(Cport[comport_number], &Cptimeouts))
+  {
+    printf("unable to set comport time-out settings\n");
+    CloseHandle(Cport[comport_number]);
+    return(1);
+  }
+
+  return(0);
+}
+
+
+int RS232_PollComport(int comport_number, unsigned char *buf, int size)
+{
+  int n;
+
+  if(size>4096)  size = 4096;
+
+/* added the void pointer cast, otherwise gcc will complain about */
+/* "warning: dereferencing type-punned pointer will break strict aliasing rules" */
+
+  ReadFile(Cport[comport_number], buf, size, (LPDWORD)((void *)&n), NULL);
+
+  return(n);
+}
+
+
+int RS232_SendByte(int comport_number, unsigned char byte)
+{
+  int n;
+
+  WriteFile(Cport[comport_number], &byte, 1, (LPDWORD)((void *)&n), NULL);
+
+  if(n<0)  return(1);
+
+  return(0);
+}
+
+
+int RS232_SendBuf(int comport_number, unsigned char *buf, int size)
+{
+  int n;
+
+  if(WriteFile(Cport[comport_number], buf, size, (LPDWORD)((void *)&n), NULL))
+  {
+    return(n);
+  }
+
+  return(-1);
+}
+
+
+void RS232_CloseComport(int comport_number)
+{
+  CloseHandle(Cport[comport_number]);
+}
+
+
+int RS232_IsCTSEnabled(int comport_number)
+{
+  int status;
+
+  GetCommModemStatus(Cport[comport_number], (LPDWORD)((void *)&status));
+
+  if(status&MS_CTS_ON) return(1);
+  else return(0);
+}
+
+
+int RS232_IsDSREnabled(int comport_number)
+{
+  int status;
+
+  GetCommModemStatus(Cport[comport_number], (LPDWORD)((void *)&status));
+
+  if(status&MS_DSR_ON) return(1);
+  else return(0);
+}
+
+
+void RS232_enableDTR(int comport_number)
+{
+  EscapeCommFunction(Cport[comport_number], SETDTR);
+}
+
+
+void RS232_disableDTR(int comport_number)
+{
+  EscapeCommFunction(Cport[comport_number], CLRDTR);
+}
+
+
+void RS232_enableRTS(int comport_number)
+{
+  EscapeCommFunction(Cport[comport_number], SETRTS);
+}
+
+
+void RS232_disableRTS(int comport_number)
+{
+  EscapeCommFunction(Cport[comport_number], CLRRTS);
+}
+
+
+#endif
+
+
+void RS232_cputs(int comport_number, const char *text)  /* sends a string to serial port */
+{
+  while(*text != 0)   RS232_SendByte(comport_number, *(text++));
+}
+
+
---
+++ Readme.txt
@@ -0,0 +1,100 @@
+GBCartRead v1.4.2 - Gameboy Cart Reader Readme
+By insideGadgets - http://www.insidegadgets.com / support@insidegadgets.com
+
+GBCartRead is an Arduino based Gameboy Cartridge Reader which uses a C program or python 
+script to interface with the Arduino. GBCartRead allows you to dump your ROM,
+save the RAM and write to the RAM.
+
+
+REQUIREMENTS
+=============
+- Arduino
+- 74HC595D SMD SO16 (2)
+- Gameboy Cart Shield - http://www.insidegadgets.com/projects/gameboy-cart-shield/
+- GBA Game Cart Slot for NDS Lite SMD (1) - http://dx.com/p/repair-parts-replacement-gba-game-cart-slot-for-nds-lite-37787
+- Arduino 8 Pin .100" Stackable Header or Female headers (2)
+- Arduino 6 Pin .100" Stackable Header or Female headers (2)
+
+OR
+
+- Breadboard or similar
+- Arduino
+- 74HC595 Shift Registers (2)
+- 470 Ohm Resistors – optional because these lines already seem  to have a high resistance already. These resistors would be for safety purposes only.
+- 10K Resistors - optional for safety purposes only
+- Gameboy Cartridge Adapter e.g: http://www.insidegadgets.com/projects/gameboy-cart-adapter/
+
+
+HOW TO USE
+=================================
+Note: It is important to insert your Gameboy Cartridge before you plug in your Arduino as you may experience
+RAM data loss if you insert when the Arduino is on.
+
+--- OPTION 1 - PYTHON PROGRAM ---
+1. Upload "GBCartRead_v1_4_2_Python.ino" to the Arduino.
+2. Download and install Python 3.2 (http://www.python.org/download/) and pySerial (http://pypi.python.org/pypi/pyserial)
+3. Open up the "GBCartRead_v1_4_2_Python.py" script by right clicking it and selecting “Edit with IDLE”.
+4. Change the 'COM2' serial port to the serial port that your Arduino is connected on. You can find this out 
+   from the Arduino software by going to Tools -> Serial port when the Arduino is plugged in
+5. Press F5 to run the script.
+6. A new window appears, press 1 to Dump the ROM, 2 to Backup your RAM or 3 to Load your RAM file. 
+   Hashes (#) will start printing every few seconds and a file called <gametitle>.gb or .sav will be created if
+   you chose option 1 or 2. If you choose option 3, it will load the save from <gametitle>.sav.
+
+
+--- OPTION 2 - C PROGRAM ---
+1. Upload "GBCartRead_v1_4_2_C.ino" to the Arduino.
+2. Open config.ini and change the COM serial port number up the top to the serial port that your Arduino
+   is connected on. You can find this out from the Arduino software by going to Tools -> Serial port
+   when the Arduino is plugged in
+3. Run gbcartread.exe in Windows or re-compile the source for your OS
+4. A new window appears, press 1 to Dump the ROM, 2 to Backup your RAM or 3 to Load your RAM file. 
+   Hashes (#) will start printing every few seconds and a file called <gametitle>.gb or .sav will be created if
+   you chose option 1 or 2. If you choose option 3, it will load the save from <gametitle>.sav.
+
+We recommended verifying your Gameboy ROM using "xgbfix.exe -v -d <your_rom.rom>" found in the project 
+called "ASMotor" (we've included it).
+
+
+REVISION HISTORY
+=================================
+v1.4.2 (14 November 2014)
+- Fixed gameTitle array length issue, thanks Frode vdM and David R
+
+v1.4.1 (3 January 2014)
+- Changed wrPin from pin 13 to A3 to suit Gameboy Cart Shield v1.2
+
+v1.4 (30 August 2013)
+- Added gbcartread.exe program for Windows (with source) which can be used instead of using the Python script
+  and is a bit faster
+
+v1.3.1 (2 May 2011)
+- Updated schematic as R19, R20 and R31 were actually acting as voltage dividers
+
+v1.3 (9 April 2011)
+- Added RAM writing ability
+- Fixed MBC2 reading RAM overflow relating to the first 4 bits not being ignored properly
+- Cleaned up bank select code to use BitRead instead of checking each bit ourselves
+- Fixed selecting wrong RAM size when reading
+- Fixed incorrect check if RAM was present before reading
+- Removed unnecessary changing of data pins to inputs after MBC was initialised when reading RAM
+
+v1.2 (2 April 2011)
+- Added RAM saving ability.
+- Now prints the amount of data read in 64K increments (i.e ###64K###128K###192K)
+- Changed <gametitle>.rom to <gametitle>.gb
+
+v1.1 (21 March 2011)
+- ROM reading is now fully automated.
+- The Game Title, MBC type, ROM size and RAM size are presented in the Python script.
+
+v1.0 (19 March 2011)
+- Reading ROM only, user to change the maximum ROM bank to read
+
+
+----------------------------------------------------------------------------
+(c) 2011-2014 by insideGadgets
+http://www.insidegadgets.com
+
+This work is licensed under a Creative Commons Attribution-NonCommercial 3.0 Unported License.
+http://creativecommons.org/licenses/by-nc/3.0/
